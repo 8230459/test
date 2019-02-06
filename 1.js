56fe04bf44e8fbdd58a71a6aa2e79bd39edf6d61
8230459 <8230459@qq.com>
2019-02-06 22:52:15 +0800

    msg

---
+++ 1.js
@@ -0,0 +1,16 @@
+const domain = 'http://localhost:3001/api'
+
+export default {
+    baseUrl: domain,
+    //登录
+    signIn: `${domain}/user/signIn`,
+    //操作数据
+    createTable: `${domain}/data/createTable`,
+    dropTable: `${domain}/data/dropTable`,
+    getRows: `${domain}/data/getRows`,
+    insertData: `${domain}/data/insertData`,
+    deleteData: `${domain}/data/deleteData`,
+    delRow: `${domain}/data/deleteDataById`,
+    modifyRow: `${domain}/data/updateData`,
+}
+
